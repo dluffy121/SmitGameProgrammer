e2ae16b9cec6a823daf2dfe6917f32b141d3c14f
dluffy121 <waghela.sonic@gmail.com>
2025-08-06 12:25:48 +0530

    - updated resume link

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,8 +33,7 @@
 		window.setTimeout(function () {
 			var f = document.createElement('iframe');
 			f.loading = "lazy";
-			f.src = "https://drive.google.com/file/d/1ogc3xFkNgYsFKthR-OTPnIzmXVvvmoCQ/view?usp=sharing";
-			// f.src = "https://www.canva.com/design/DAFRMkLiCk8/lP5rEDitFqwJXkpCDjbpFA/view?embed";
+			f.src = "https://drive.google.com/file/d/1GZwgJgG8e7Qg3uPlb1PzRv0bAvucK82d/preview?autoplay=1";
 			f.width = '100%';
 			f.height = '950em';
 			f.sandbox = "allow-scripts allow-popups"
