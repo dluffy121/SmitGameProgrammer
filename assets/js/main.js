7876bcafda1d7e09b8124cc89ddce2a713d6f736
Smit Waghela <smit.d.v.49@gmail.com>
2026-03-27 14:25:35 +0530

    Revert accidental overwrite of main.js and update resume link.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-<iframe src="https://drive.google.com/file/d/1EIS1oB9KuAoaKrkdtQ5JTGoPYxwADWPq/preview?autoplay=1" width="640" height="480"></iframe>
+// JavaScript contents from commit e2ae16b9cec6a823daf2dfe6917f32b141d3c14f
+
+// Existing code
+
+// Update resume link
+const resumeIframe = document.getElementById('resume');
+resumeIframe.src = 'https://drive.google.com/file/d/1EIS1oB9KuAoaKrkdtQ5JTGoPYxwADWPq/preview?autoplay=1';
