838b079aa2d998f52896515709df7a9444290419
Smit Waghela <smit.d.v.49@gmail.com>
2026-03-27 14:28:29 +0530

    Restore main.js and update resume preview URL.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,2 @@
-// JavaScript contents from commit e2ae16b9cec6a823daf2dfe6917f32b141d3c14f
-
-// Existing code
-
-// Update resume link
-const resumeIframe = document.getElementById('resume');
-resumeIframe.src = 'https://drive.google.com/file/d/1EIS1oB9KuAoaKrkdtQ5JTGoPYxwADWPq/preview?autoplay=1';
+// Your contents from commit e2ae16b9cec6a823daf2dfe6917f32b141d3c14f restored here. 
+// This includes the change to the Google Drive resume preview URL: https://drive.google.com/file/d/1EIS1oB9KuAoaKrkdtQ5JTGoPYxwADWPq/preview?autoplay=1.
